06dabf433b469c4892368fe775ab2406b352863c
Shubham Vishwakarma <shubhamviswa2000@gmail.com>
2024-11-27 08:20:39 +0530

    Create first_and_last_occurance.js

---
+++ Searching_and_Sorting/Binary_Search/first_and_last_occurance.js
@@ -0,0 +1,42 @@
+//  First and Last Occurrences of x in Array 
+
+let arr = [2,8,8,8,8,8,11,13];
+let target = 8;
+
+function first_last_occurance(arr, target){
+
+  let first = -1;
+  let last = -1;
+
+  // finding the first occurrence
+  let lo = 0;
+  let hi = arr.length - 1;
+  while (lo <= hi){
+    let mid = Math.floor((lo + hi) / 2);
+    if(arr[mid] === target) {
+      first = mid;
+      hi = mid - 1;
+    }else if(arr[mid] > target){
+      hi = mid - 1;    
+    }else{
+      lo = mid + 1;
+    }
+  }
+
+  //  finding the last occurence
+
+  while(lo<=hi){
+    let mid = Math.floor((lo+hi)/2);
+    if(arr[mid] === target){
+      last = mid;
+      lo = mid + 1;
+    }else if(arr[mid] > target){
+      hi = mid - 1;
+    }else{
+      lo = mid + 1;
+    }
+  }
+
+  return {first , last};
+  
+}
