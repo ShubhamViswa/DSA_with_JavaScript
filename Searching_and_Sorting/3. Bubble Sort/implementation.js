59347956b8bb8dff00098ce8efe8725fc62c843a
Shubham Vishwakarma <shubhamviswa2000@gmail.com>
2024-12-03 10:03:20 +0530

    Update implementation.js

--- Searching_and_Sorting/3. Bubble Sort/implementation.js
+++ Searching_and_Sorting/3. Bubble Sort/implementation.js
@@ -1,1 +1,18 @@
 // implementing bubble sort
+
+
+let arr =  [34, 7, 89, 22, 45,1,2,4,3,44];
+let n = arr.length;
+for(let i = 0; i < n-1; i++){
+  for(let j = 0; j < n-i-1; j++){
+    if(arr[j] > arr[j+1]){
+      
+      // swap 
+      let temp = arr[j];
+      arr[j] = arr[j+1];
+      arr[j+1] = temp;
+    }
+  }
+}
+
+console.log(arr);
