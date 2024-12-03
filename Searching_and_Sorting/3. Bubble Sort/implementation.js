466c97e593aa8e7634701b7d59d1cde40781a311
Shubham Vishwakarma <shubhamviswa2000@gmail.com>
2024-12-03 10:17:29 +0530

    Update implementation.js

--- Searching_and_Sorting/3. Bubble Sort/implementation.js
+++ Searching_and_Sorting/3. Bubble Sort/implementation.js
@@ -20,6 +20,28 @@ for(let i = 0; i < n-1; i++){
 
 console.log(arr);
 
+// Optimized Bubble sort
+
+let arr =  [34, 7, 89, 22, 45,1,2,4,3,44];
+let n = arr.length;
+for(let i = 0; i < n-1; i++){
+  let swapped = false;
+  for(let j = 0; j < n-i-1; j++){
+    if(arr[j] > arr[j+1]){
+      
+        // swap 
+      
+      [arr[j] , arr[j+1]] = [arr[j+1] , arr[j]]
+      swapped = true;
+    //   let temp = arr[j];
+    //   arr[j] = arr[j+1];
+    //   arr[j+1] = temp;
+    }
+  }
+  if(!swapped) = false;
+}
+
+console.log(arr);
 
 //  Time complexity of this code is O(n^2);
 //  Space Complexity is auxaliry Space O(1);
