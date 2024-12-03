fa65f147d8a285d562ddaad178fcc698af68f423
Shubham Vishwakarma <shubhamviswa2000@gmail.com>
2024-12-03 13:22:43 +0530

    Update Implementation.js

--- Searching_and_Sorting/5. InsertionSort/Implementation.js
+++ Searching_and_Sorting/5. InsertionSort/Implementation.js
@@ -1,1 +1,16 @@
 // Implementation of Insertion Sorting Algorithm
+//  
+// In insertion sort we pick the first element from our array and put that element at the it's correct position by comparing from the index[0] to index[n-1]
+// 
+// 
+
+function InsertionSort(arr){
+  let n = arr.length;
+  for(let i = 0; i < n; i++){
+    for(let j = 0; j < n; j++){
+      if(arr[j] > arr[j+1]){
+        [arr[j] , arr[j+1]] = [arr[j+1] , arr[j]];
+      }
+    }
+  }
+}
